71a30ec7982ade8eb98dc59ad801e6b6b3ede377
Carlos <contact@carloscolin.me>
2021-07-16 19:58:03 -0500

    made instance of Vue object and declared the object

---
+++ app.js
@@ -0,0 +1,44 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            name: 'Bitcoin',
+            symbol: 'BTC',
+            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+            changePercent: 10,
+            price: 8400,
+            pricesWithDays: [
+                { day: 'Lunes', value: 8400 },
+                { day: 'Martes', value: 7900 },
+                { day: 'Miercoles', value: 8200 },
+                { day: 'Jueves', value: 9000 },
+                { day: 'Viernes', value: 9400 },
+                { day: 'Sabado', value: 10000 },
+                { day: 'Domingo', value: 10200 },
+            ],
+
+            showPrices: false,
+            color: "f4f4f4"
+        }
+    },
+
+    computed: {
+        title() {
+            return `${this.name} - ${this.symbol}`;
+        }
+    },
+
+    watch: {
+        showPrices(newVal, oldVal) {
+            console.log(newVal, oldVal);
+        }
+    },
+    
+    methods: {
+        toggleShowPrices () {
+            this.showPrices = !this.showPrices;
+
+            this.color = this.color.split('').reverse().join('');
+        }
+    }
+});
